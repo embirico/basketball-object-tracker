3b94247a76947ad8c1790e7d7f96036ca592b134
gpoon <gpoon47@stanford.edu>
2014-03-03 21:51:37 -0800

    1d histogram

--- src/line_pixel_detection.py
+++ src/line_pixel_detection.py
@@ -8,18 +8,30 @@
 import cv2
 import numpy as np
 
-
-
-
 def convertToYCrCb(imageName):
 	img = cv2.imread(imageName)
-	newImg = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+	newImg = cv2.cvtColor(img,cv2.COLOR_BGR2YCR_CB)
 	# newImg = cv2.cvtColor(img,cv2.COLOR_RGB2YCR_CB)
 	# gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+	h = np.zeros((300,256,3))
+	bins = np.arange(256).reshape(256,1)
+	color = [ (255,0,0),(0,255,0),(0,0,255) ]
 
-	cv2.imshow('bitch',newImg)
-	if cv2.waitKey(0) & 0xff == 27:
-		cv2.destroyAllWindows()
+	y, cr, cb = newImg[:,:,0].copy(), newImg[:,:,1].copy(), newImg[:,:,2].copy()
+	for item,col in enumerate(color):
+	    hist_item = cv2.calcHist([newImg],[item],None,[256],[0,255])
+	    cv2.normalize(hist_item,hist_item,0,255,cv2.NORM_MINMAX)
+	    hist=np.int32(np.around(hist_item))
+	    pts = np.column_stack((bins,hist))
+	    cv2.polylines(h,[pts],False,col)
+
+	h=np.flipud(h)
+
+	cv2.imshow('colorhist',h)
+	cv2.waitKey(0)
+	# cv2.imshow('bitch',newImg)
+	# if cv2.waitKey(0) & 0xff == 27:
+	# 	cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
