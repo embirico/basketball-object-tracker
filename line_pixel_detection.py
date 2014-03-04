2cffe64424fca3c963cad83c98b7a24636acf1ca
Alexander Embiricos <alexanderembiricos@gmail.com>
2014-03-03 18:09:54 -0800

    research into farin

---
+++ line_pixel_detection.py
@@ -0,0 +1,6 @@
+# First, use Liu "Playfield detection" to get a GMM of court color
+# Then, mask non-court
+# Then, detect line pixels
+# Then, get lines
+# Then, find features from line interections
+# Then compute optimal H
