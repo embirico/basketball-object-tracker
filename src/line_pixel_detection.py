551de566bacb3ec987c2a35b275c3175b86e9fb9
Alexander Embiricos <alexanderembiricos@gmail.com>
2014-03-04 00:05:31 -0800

    Early masking

--- src/line_pixel_detection.py
+++ src/line_pixel_detection.py
@@ -12,9 +12,8 @@ from matplotlib import pyplot as plt
 
 from collections import deque
 
-def getColorPeaks(imageName):
-	img = cv2.imread(imageName)
-	img = cv2.cvtColor(img,cv2.COLOR_BGR2YCR_CB)
+def getColorPeaks(image_name):
+	img = cv2.cvtColor(cv2.imread(image_name), cv2.COLOR_BGR2YCR_CB)
 
 	hist = cv2.calcHist([img], [1,2], None, [256,256], [0,256, 0,256])
 
@@ -31,18 +30,25 @@ def getColorPeaks(imageName):
 	print "{} counts similar to {}".format(sum1, peak1_idx)
 	print "{} counts similar to {}".format(sum2, peak2_idx)
 
-	show_hist([hist, subtracted_hist, _])
+	# show_hist([hist, subtracted_hist, _])
+
+	# if False:#sum1 >= sum2:
+	# 	return connected_hist1
+	# else:
+	# 	return connected_hist2
+
+	return connected_hist1 | connected_hist2
 
 
 # BTW etc is the total count and the 'subtracted new histogram'
 def get_connected_hist(hist, peak_idx, thresh=.05):
-	connected_hist = np.zeros(hist.shape)
+	connected_hist = set()
 	sum_val = 0
 	subtracted_hist = np.copy(hist)
 
 	min_passing_val = thresh * hist[peak_idx]
 
-	connected_hist[peak_idx] = 1
+	connected_hist.add(peak_idx)
 	sum_val	+= hist[peak_idx]
 	subtracted_hist[peak_idx] = 0
 	queue = deque([peak_idx])
@@ -59,14 +65,13 @@ def get_connected_hist(hist, peak_idx, thresh=.05):
 			toAdd.append((x, y+1))
 
 		for idx in toAdd:
-			if not connected_hist[idx] and hist[idx] >= min_passing_val:
-				connected_hist[idx] = 1
+			if idx in connected_hist and hist[idx] >= min_passing_val:
+				connected_hist.add(idx)
 				sum_val += hist[idx]
 				subtracted_hist[idx] = 0
 				queue.append(idx)
 
 	return connected_hist, sum_val, subtracted_hist
-
 
 
 def oneDimHists(imageName):
@@ -108,6 +113,19 @@ def show_hist(hist_list):
 	plt.show()
 
 
+def create_court_mask(image_name, dominant_colorset):
+	img = cv2.cvtColor(cv2.imread(image_name), cv2.COLOR_BGR2YCR_CB)
+	for row in xrange(img.shape[0]):
+		for col in xrange(img.shape[1]):
+			idx = (row, col)
+			_, cr, cb = img[idx]
+			if (cr, cb) not in dominant_colorset:
+				img[idx] = (0,128,128)
+
+	show_image(cv2.cvtColor(img, cv2.COLOR_YCR_CB2BGR))
+
+
 if __name__ == '__main__':
-	imageName = 'images/5993.jpg'
-	getColorPeaks(imageName)
+	image_name = 'images/5993.jpg'
+	dominant_colorset = getColorPeaks(image_name)
+	create_court_mask(image_name, dominant_colorset)
