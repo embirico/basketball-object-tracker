2aba7f429df3acf536266426c0f669e650ae37dd
gpoon <gpoon47@stanford.edu>
2014-03-11 03:21:14 -0700

    fixed finding intersection

--- src/find_intersection_hough_lines.py
+++ src/find_intersection_hough_lines.py
@@ -6,14 +6,14 @@ def find_intersecting_point_hough_lines(theta_sideline, rho_sideline, theta_base
 	x0_sideline = a_sideline*rho_sideline
 	y0_sideline = b_sideline*rho_sideline
 	slope_sideline = a_sideline / -b_sideline
-	intercept_sideline = rho_sideline * (np.sin(theta_sideline) + (np.cos(theta_sideline) / np.sin(theta_sideline)))
+	intercept_sideline = rho_sideline / np.sin(theta_sideline)#rho_sideline * (np.sin(theta_sideline) + (np.cos(theta_sideline) / np.sin(theta_sideline)))
 
 	a_baseline = np.cos(theta_baseline)
 	b_baseline = np.sin(theta_baseline)
 	x0_baseline = a_baseline*rho_baseline
 	y0_baseline = b_baseline*rho_baseline
 	slope_baseline = a_baseline / -b_baseline
-	intercept_baseline = rho_baseline * (np.sin(theta_baseline) + (np.cos(theta_baseline) / np.sin(theta_baseline)))
+	intercept_baseline = rho_baseline / np.sin(theta_baseline) #rho_baseline * (np.sin(theta_baseline) + (np.cos(theta_baseline) / np.sin(theta_baseline)))
 	
 	x_intersection = (intercept_baseline - intercept_sideline) / (slope_sideline - slope_baseline)
 	y_intersection = x_intersection * slope_sideline + intercept_sideline
