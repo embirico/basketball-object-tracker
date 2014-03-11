ba4bd7596666ff098ab09bac24e7cd91989ee0e2
Alexander Embiricos <alexanderembiricos@gmail.com>
2014-03-11 00:59:24 -0700

    hole filling

--- src/hough.py
+++ src/hough.py
@@ -8,39 +8,55 @@ from matplotlib import pyplot as plt
 import colors # local reference
 
 
-def find_top_boundary(court_mask):
-	# top_line_set = set()
-	# for col in xrange(img.shape[1]):
-	# 	for row in xrange(img.shape[0]):
-	# 		if court_mask[row][col] != (0,128,128):
-	# 			top_line_set.append((row,col))
-	# 			break
-
-	# # RANSAC (projection threshold ~0.5 of horizontal baseline)
-	# best_top_line = find_top_line(top_line_set)
-	top_line_only = np.copy(court_mask)
-	for col in xrange(court_mask.shape[1]):
-		top_found = False
-		for row in xrange(court_mask.shape[0]):
-			if top_found:
-				top_line_only[row][col] = (0,128,128)
-			else:
-				if not np.array_equal(top_line_only[row][col], np.array([0,128,128])):
-					top_found = True
-
-	# Hough transform to find top boundary (doesn't work that well)
-	best_top_line = hough_find_top_line(top_line_only)
-	# RANSAC (projection threshold ~0.5 of horizontal baseline)
-	# best_top_line = ransac_find_top_line(top_line_only)
+def fill_holes_with_contour_filling(gray):
+  filled = gray.copy()
+  contour, _ = cv2.findContours(filled,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+  for cnt in contour:
+    cv2.drawContours(gray, [cnt], 0, 255, -1)
+  return filled
 
 
 if __name__ == '__main__':
-	# image_root = 'images/6175'
-	image_root = 'images/5993'
-	image_ext = '.jpg'
-	image_name = image_root + image_ext
-	court_mask = colors.create_court_mask(image_name, binary=True)
-	court_mask = colors.ycbcr_to_binary(court_mask)
-	plt.imshow(court_mask)
-	plt.show()
-	# edge = cv2.Canny(gray, thrs1, thrs2, apertureSize=5)
+  # image_root = 'images/6175'
+  image_root = 'images/5993'
+  image_ext = '.jpg'
+  image_name = image_root + image_ext
+  court_mask = colors.create_court_mask(image_name, binary_gray=True)
+  cv2.imwrite('images/mask.jpg', court_mask)
+  flooded = fill_holes_with_contour_filling(court_mask)
+  cv2.imwrite('images/mask_flooded3.jpg', flooded)
+  # court_mask = colors.ycbcr_to_binary(court_mask)
+  # plt.imshow(court_mask)
+  # plt.show()
+  # edge = cv2.Canny(gray, thrs1, thrs2, apertureSize=5)
+
+
+# Unused code kept for the report etc -----------
+
+
+# See https://github.com/Itseez/opencv/blob/master/samples/python2/floodfill.py
+def flood_holes(gray):
+  # Setup
+  flooded = gray.copy()
+  h, w = gray.shape[:2]
+  mask = np.zeros((h+2, w+2), np.uint8)
+  connectivity = 4
+  flags = connectivity
+  lo = 128
+  hi = 128
+
+  # Do
+  seed_pt = (350, 350)
+  cv2.floodFill(flooded, mask, seed_pt, (255,), lo, hi, flags)
+  flooded = colors.gray_to_bgr(flooded)
+  cv2.circle(flooded, seed_pt, 2, (0, 0, 255), -1)
+
+  return flooded
+
+
+def fill_holes_with_img_opening(gray):
+  gray = cv2.bitwise_not(gray)
+  kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
+  res = cv2.morphologyEx(gray,cv2.MORPH_OPEN,kernel)
+  res = cv2.bitwise_not(res)
+  return res
