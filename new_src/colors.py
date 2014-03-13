584232cfd5d4296146d90bd83e7c51577fa7eb38
gpoon <gpoon47@stanford.edu>
2014-03-12 19:59:50 -0700

    fixed error in colors.py

--- new_src/colors.py
+++ new_src/colors.py
@@ -17,26 +17,27 @@ BGR_BLACK = (0,0,0)
 # Exported code ---------------------------------
 
 def create_court_mask(_bgr_img, dominant_colorset, binary_gray=False):
-	for row in xrange(_bgr_img.shape[0]):
-		for col in xrange(_bgr_img.shape[1]):
+	img = cv2.cvtColor(_bgr_img, cv2.COLOR_BGR2YCR_CB)
+	for row in xrange(img.shape[0]):
+		for col in xrange(img.shape[1]):
 			idx = (row, col)
-			_, cr, cb = _bgr_img[idx]
+			_, cr, cb = img[idx]
 			if (cr, cb) not in dominant_colorset:
-				_bgr_img[idx] = (0,128,128)
+				img[idx] = (0,128,128)
 			elif binary_gray:
-				_bgr_img[idx] = (255,128,128)
+				img[idx] = (255,128,128)
 
-	return ycbcr_to_gray(_bgr_img) if binary_gray else _bgr_img
+	return ycbcr_to_gray(img) if binary_gray else img
 
 
 def get_dominant_colorset(_bgr_img, thresh=0.02, ignore_crowd=True,
 	peak_num=1):
-	ycrcb_img = cv2.cvtColor(_bgr_img, cv2.COLOR_BGR2YCR_CB)
+	img = cv2.cvtColor(_bgr_img, cv2.COLOR_BGR2YCR_CB)
 
 	if ignore_crowd:
-		ycrcb_img = ycrcb_img[CROWD_TOP_HEIGHT_FRACTION*ycrcb_img.shape[0] : -CROWD_BOTTOM_HEIGHT_FRACTION*ycrcb_img.shape[0]]
+		img = img[CROWD_TOP_HEIGHT_FRACTION*img.shape[0] : -CROWD_BOTTOM_HEIGHT_FRACTION*img.shape[0]]
 
-	hist = cv2.calcHist([ycrcb_img], [1,2], None, [256,256], [0,256, 0,256])
+	hist = cv2.calcHist([img], [1,2], None, [256,256], [0,256, 0,256])
 
 	peak1_flat_idx = np.argmax(hist)
 	peak1_idx = np.unravel_index(peak1_flat_idx, hist.shape)
