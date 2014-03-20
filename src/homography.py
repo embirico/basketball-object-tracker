1e890ec7d770280769bd6c3aa4f2fbeaf5b642c5
Alexander Embiricos <alexanderembiricos@gmail.com>
2014-03-19 23:19:24 -0700

    Consolidate homography files

--- src/homography.py
+++ src/homography.py
@@ -54,64 +54,55 @@ class NbaCourt:
 
 
 def compute_homography(image_pts, side):
-	a = np.zeros((8,8))
-	b = np.zeros((8,1))
+	a = np.zeros((12,8))
+	b = np.zeros((12,1))
 	row = 0
 	for loc in image_pts:
-		xw = image_pts[loc]['x']
-		yw = image_pts[loc]['y']
-		x, y = NbaCourt.get_loc_coords(loc, side)
-		# x = image_pts[loc]['x']
-		# y = image_pts[loc]['y']
-		# xw, yw = NbaCourt.get_loc_coords(loc, side)
-		a[row,:] = [x, y, 1, 0, 0, 0, x*xw, y*xw]
-		b[row] = xw
+		x = image_pts[loc]['x']
+		y = image_pts[loc]['y']
+		xw, yw = NbaCourt.get_loc_coords(loc, side)
+		a[row,:] = [xw,yw,1, 0,0,0, 0,0]
+		b[row] = x
 		row += 1
-		a[row,:] = [0, 0, 0, x, y, 1, x*yw, y*yw]
-		b[row] = yw
+		a[row,:] = [0,0,0, xw,yw,1, 0,0]
+		b[row] = y
+		row += 1
+		a[row,:] = [0,0,0, 0,0,0, xw,yw]
+		b[row] = 0
 		row += 1
 	h_vals = np.linalg.pinv(a).dot(b)
 	h_vals = np.append(h_vals, [[1]], axis=0) # append h_{22} = 1
 	h = np.reshape(h_vals, (3, 3))
-
-	# h[0:2,2] = -h[0:2,2] #TODO remove
-
-	# h[1:3,0] = -h[1:3,0]
-	# h[0,1] = -h[0,1]
-	# h[2,1] = -h[2,1]
-
-	# h[1,1] = -h[1,1]
 	return h
 
 
 if __name__ == '__main__':
-	img_num = 6233
+	img_num = 5993
 	img_num_str = str(img_num)
-	side = 'left'
+	side = 'right'
 	# img_num_str = '5993'
 	with open('manual_points.json', 'r') as f:
 		manual_data = json.load(f)
 	with open('computed_points.json', 'r') as f:
-		computed_pts = json.load(f)
-	manual_data_img = manual_data[img_num_str]
+		computed_data = json.load(f)
+	data_img = manual_data[img_num_str]
 
-	h = compute_homography(manual_data_img, side)
+	h = compute_homography(data_img, side)
 	print h
 
 	img_obj = ImageObject('images/{}.jpg'.format(img_num))
 
 	manual_points = []
 	reprojected_pts = []
-	for loc in manual_data_img:
+	for loc in data_img:
 		print loc
-		manual_point = manual_data_img[loc]
+		manual_point = data_img[loc]
 		manual_points.append((manual_point['x'], manual_point['y']))
 		xw, yw = NbaCourt.get_loc_coords(loc, side)
 		print [[xw], [yw], [1]]
 		p = h.dot([[xw], [yw], [1]])
-		# p = p / p[2]
 		p = p * p[2]
-		print manual_data_img[loc]
+		print data_img[loc]
 		print p
 		reprojected_pts.append((p[0], p[1]))
 
